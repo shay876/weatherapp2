02a6304eed05476d89cee5a705d7a86d6fe3e09c
shay876 <111309646+shay876@users.noreply.github.com>
2022-10-02 14:56:20 -0600

    add and link search engine api

--- script.js
+++ script.js
@@ -42,7 +42,18 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "f1eac4a368d3b697e5d93c0ab2e3fcbc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Calgary&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "f1eac4a368d3b697e5d93c0ab2e3fcbc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
