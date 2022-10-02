b4e2b6096a66e63ea0da201e758f7af43368146c
shay876 <111309646+shay876@users.noreply.github.com>
2022-10-02 12:56:33 -0600

    add humidity and wind speed

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windspeedElement = document.querySelector("#windspeed");
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "f1eac4a368d3b697e5d93c0ab2e3fcbc";
